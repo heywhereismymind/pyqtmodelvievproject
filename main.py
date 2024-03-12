c7b02ff3dc4755d24041d01113d12cf32e9e37f4
Ivaniu_YI <Ivaniu_YI@npc.by>
2024-03-12 09:45:46 +0300

    customize table contacts

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 import sys
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
-# Create the connection
-con = QSqlDatabase.addDatabase("QSQLITE")
-con.setDatabaseName("contacts.sqlite")
-# Open the connection
-if not con.open():
-    print("Database Error: %s" % con.lastError().databaseText())
-    sys.exit(1)
-# Create a query and execute it right away using .exec()
-createTableQuery = QSqlQuery()
-createTableQuery.exec("""
-CREATE TABLE contacts (
-id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-name VARCHAR(40) NOT NULL,
-job VARCHAR(50),
-email VARCHAR(40) NOT NULL)
-""")
-print(con.tables())
+from PyQt5.QtWidgets import (
+                            QApplication,
+                            QMainWindow,
+                            QMessageBox,
+                            QTableWidget,
+                            QTableWidgetItem,
+                            )
 
-# Creating a query for later execution using .prepare()
-insertDataQuery = QSqlQuery()
-insertDataQuery.prepare(
-"""
-INSERT INTO contacts (
-name,
-job,
-email
-)
-VALUES (?, ?, ?)
-"""
-)
-# Sample data
-data = [
-("Петя", "Senior Web Developer", "petya@example.com"),
-("Лера", "Project Manager", "lera@example.com"),
-("Денис", "Data Analyst", "denis@example.com"),
-("Катя", "Senior Python Developer", "ket@example.com"),
-]
-# Use .addBindValue() to insert data
-for name, job, email in data:
-    insertDataQuery.addBindValue(name)
-    insertDataQuery.addBindValue(job)
-    insertDataQuery.addBindValue(email)
-    insertDataQuery.exec()
+
+class Contacts(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("QTableView Example")
+        self.resize(450, 250)
+        # Set up the view and load the data
+        self.view = QTableWidget()
+        self.view.setColumnCount(4)
+        self.view.setHorizontalHeaderLabels(["ID", "Name", "Job", "Email"])
+        query = QSqlQuery("SELECT id, name, job, email FROM contacts")
+        while query.next():
+            rows = self.view.rowCount()
+            self.view.setRowCount(rows + 1)
+            self.view.setItem(rows, 0, QTableWidgetItem(str(query.value(0))))
+            self.view.setItem(rows, 1, QTableWidgetItem(query.value(1)))
+            self.view.setItem(rows, 2, QTableWidgetItem(query.value(2)))
+            self.view.setItem(rows, 3, QTableWidgetItem(query.value(3)))
+            self.view.resizeColumnsToContents()
+            self.setCentralWidget(self.view)
+
+
+def create_connection() -> bool:
+    con = QSqlDatabase.addDatabase("QSQLITE")
+    con.setDatabaseName("contacts.sqlite")
+    if not con.open():
+        QMessageBox.critical(
+        None,
+        "QTableView Example - Error!",
+        "Database Error: %s" % con.lastError().databaseText(),
+        )
+        return False
+    return True
+
+
+app = QApplication(sys.argv)
+if not create_connection():
+    sys.exit(1)
+win = Contacts()
+win.show()
+sys.exit(app.exec_())
